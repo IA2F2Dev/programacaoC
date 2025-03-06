8555613e2b5794faa3dff9ef27c331bfed090711
IA2F2Dev <isaias.angelo27@gmail.com>
2025-03-06 09:55:40 +0000

    Completo.

--- DobrarValor.c
+++ DobrarValor.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main()
+{
+
+    double n;
+    int x;
+
+    printf("Digite o número a ser Multiplicado: ");
+    scanf("%lf", &n);
+    printf("Digite quantas vezes número deve se Multiplicar: ");
+    scanf("%d", &x);
+
+    for (int i = 0; i < x; i++)
+    {
+        n = n + n;
+        printf("Resultado após %d iterações: %.2lf\n", i + 1, n);
+    }
+
+
+    return 0;
+}
